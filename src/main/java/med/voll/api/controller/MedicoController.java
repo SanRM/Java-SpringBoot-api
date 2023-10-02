76e049aaac4cab1dbffbd85addba7a2188564b40
SanRM <santiagoromom95@gmail.com>
2023-10-02 17:44:21 -0500

    first commit

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,64 @@
+package med.voll.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.medico.DatosActualizarMedico;
+import med.voll.api.medico.DatosListadoMedico;
+import med.voll.api.medico.DatosRegistroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+    
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @PostMapping
+    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico){
+        medicoRepository.save(new Medico(datosRegistroMedico));
+    }
+
+    @GetMapping
+    public Page<DatosListadoMedico> listadoMedicos(@PageableDefault(size = 2) Pageable paginacion) {
+        // return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
+        return medicoRepository.findByActivoTrue(paginacion).map(DatosListadoMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void actualizarMedico(@RequestBody @Valid DatosActualizarMedico DatosActualizarMedico){
+        Medico medico = medicoRepository.getReferenceById(DatosActualizarMedico.id());
+        medico.actualizarDatos(DatosActualizarMedico);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    //Delete logico
+    public void eliminarMetodo(@PathVariable Long id){
+        Medico medico = medicoRepository.getReferenceById(id);
+        medico.desactivarMedico();
+    }
+
+    //Delete en base de datos
+    // public void eliminarMetodo(@PathVariable Long id){
+    //     Medico medico = medicoRepository.getReferenceById(id);
+    //     medicoRepository.delete(medico);
+    // }
+
+}
